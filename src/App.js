066e8642ba891e9d3bd04d6ca84727a4bc8a20af
Artem66 <melodpi@gmail.com>
2019-08-15 15:26:43 +0300

    added react router to project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react';
-import './App.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
 
 export default class App extends React.Component {
   render() {
     return (
-      <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          {'2GR'}
-        </Navbar.Brand>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">О нас</Nav.Link>
-            <Nav.Link href="#pricing">Цены</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={ 2 } href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>    
-      </>
+      <Router>
+        <Header />
+        <Switch>          
+          <Route exact path="/" component={ Home } />
+          <Route path="/about/" component={ About } />          
+        </Switch>        
+      </Router>
     );
   }
 }

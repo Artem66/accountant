b150e248788caee3b38cf18c16df818b53a480b2
Artem66 <melodpi@gmail.com>
2020-05-15 15:54:24 +0300

    Updates

---
+++ precache-manifest.c652228e8965e66586e5ed32de1b5986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edd54a674e44574450e5e80fac1be2c",
+    "url": "/accountant/index.html"
+  },
+  {
+    "revision": "8830b551236e25096608",
+    "url": "/accountant/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2064e28f354c314c06b1",
+    "url": "/accountant/static/css/main.1558b249.chunk.css"
+  },
+  {
+    "revision": "8830b551236e25096608",
+    "url": "/accountant/static/js/2.3c159248.chunk.js"
+  },
+  {
+    "revision": "2064e28f354c314c06b1",
+    "url": "/accountant/static/js/main.b3640c3a.chunk.js"
+  },
+  {
+    "revision": "28aeb089b7a515096122",
+    "url": "/accountant/static/js/runtime~main.947ae5a8.js"
+  }
+]);
